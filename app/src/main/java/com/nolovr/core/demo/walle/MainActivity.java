1ca60c6e744b74ac7e7def28364369575ae356ac
dell-liu.lg@163.com <liu.lg@163.com>
2021-09-15 22:03:57 +0800

    验证 jar包合并，ok

--- app/src/main/java/com/nolovr/core/demo/walle/MainActivity.java
+++ app/src/main/java/com/nolovr/core/demo/walle/MainActivity.java
@@ -9,7 +9,6 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
-
 import com.meituan.android.walle.WalleChannelReader;
 
 public class MainActivity extends AppCompatActivity {
